49dadbb69039665ff1ef597048de26a0e9f22273
kazuho home desktop <kazuho.kobayashi.ynu@gmail.com>
2026-01-14 00:02:39 +0900

    created localization package

--- src/patasmonkey_localization/setup.py
+++ src/patasmonkey_localization/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'patasmonkey_localization'
 
@@ -10,13 +12,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mujin',
-    maintainer_email='mujin',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='kazuho.kobayashi.ynu@gmail.com',
+    description='localization for patasmonkey UGV',
+    license='MIT',
     extras_require={
         'test': [
             'pytest',
