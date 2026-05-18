3e3c91076c6ce39658651b8063a579448afcff0a
kazuho <kazuho.kobayashi.ynu@gmail.com>
2026-05-18 22:18:55 +0900

    added topic for motor status

--- src/patasmonkey_vehicle_interface/setup.py
+++ src/patasmonkey_vehicle_interface/setup.py
@@ -7,13 +7,16 @@ setup(
     name=package_name,
     version="0.0.0",
     packages=find_packages(
-        include=["patasmonkey_vehicle_interface", "patasmonkey_vehicle_interface.*"]
+        include=["patasmonkey_vehicle_interface",
+                 "patasmonkey_vehicle_interface.*"]
     ),
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/ament_index/resource_index/packages",
+         ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/config", glob("config/*.yaml")),
         ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+        ("share/" + package_name + "/msg", glob("msg/*.msg"))
     ],
     install_requires=["setuptools"],
     zip_safe=True,
